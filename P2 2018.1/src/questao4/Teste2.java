d3ff6024321afd66941152e888ee693a3a5e7f03
Lari <88565631+romugaboo@users.noreply.github.com>
2022-08-05 11:42:48 -0300

    questão 3 da 2018.2 adicionada

---
+++ P2 2018.1/src/questao4/Teste2.java
@@ -0,0 +1,30 @@
+package questao4;
+
+public class Teste2 {
+	public static void main(String[] args) {
+		System.out.println("inicio");
+		metodo1();
+		System.out.println("fim");
+	}
+
+	public static void metodo1() {
+		System.out.println("inicio do metodo1");
+		metodo2();
+		System.out.println("fim do metodo1");
+	}
+
+	public static void metodo2() {
+		System.out.println("inicio do metodo2");
+		int[] array = new int[2];
+		for (int i = 0; i <= 4; i++) {
+			try {
+				array[i] = i;
+				System.out.println(i);
+			}
+			catch (RuntimeException e) {
+				System.out.println(e);
+			}
+		}
+		System.out.println("fim do metodo2");
+	}
+}
